9415ad60a0761df171d5cc0ab9b7434332c9f1c7
Emma Albury <emma_albury@hotmail.com>
2018-12-05 16:21:22 +0000

    create check_length method and test

--- Projects/Anagrams/lib/anagram.rb
+++ Projects/Anagrams/lib/anagram.rb
@@ -1,7 +1,24 @@
 class Anagrams
 
+  def initialize
+    @length_list = []
+  end
+
   def split(word)
     word.split('')
   end
 
+  def check_length(word, list)
+    list.each do |w|
+      if w.length == word.length
+        @length_list << w
+      end
+    end
+    @length_list
+  end
+
 end
+
+# ana = Anagrams.new
+# ana.check_length('mace', ['came', 'dog', 'Emma', 'base', 'best', 'pot',\
+#    'sloppy', 'camel', 'remain', 'scare'])
--- Projects/Anagrams/spec/anagram_spec.rb
+++ Projects/Anagrams/spec/anagram_spec.rb
@@ -4,12 +4,23 @@ describe Anagrams do
 
   let(:anagram) { described_class.new }
 
-  describe "#split" do
-    it "splits word string" do
-      # allow(anagram).to receive(:split) {'dog'}
-      expect(anagram.split('dog')).to eq(['d', 'o', 'g'])
+  context 'splits' do
+    describe "#split" do
+      it "splits word string" do
+        expect(anagram.split('dog')).to eq(['d', 'o', 'g'])
+      end
     end
+
+    # describe '#split_list' do
+    #   it "splits list"
+    # end
   end
 
+  describe "#check_length" do
+    it "matches length of words" do
+      expect(anagram.check_length('mace', ['came', 'dog', 'Emma', 'base', 'best', 'pot',\
+         'sloppy', 'camel', 'remain', 'scare'])).to eq ['came', 'Emma', 'base', 'best']
+    end
+  end
 
 end

06a94181e66cd01d97c121e7eaaf010611c11800
Emma Albury <emma_albury@hotmail.com>
2018-12-12 21:51:34 +0000

    create sort_letters method

--- Projects/Anagrams/lib/anagram.rb
+++ Projects/Anagrams/lib/anagram.rb
@@ -2,10 +2,24 @@ class Anagrams
 
   def initialize
     @length_list = []
+    @split_word = []
+    @sorted_words = {}
+    @sorted_list =[]
   end
 
   def split(word)
-    word.split('')
+    @split_word << word.split('')
+    @split_word
+  end
+
+  def sort_letters(word)
+    sorted_word = word.chars.sort(&:casecmp).join
+    puts sorted_word
+    sorted_words = Hash.new[sorted_word]
+    @length_list.each do |word|
+      @sorted_list << word.chars.sort(&:casecmp).join
+    end
+    puts @sorted_list
   end
 
   def check_length(word, list)
@@ -19,6 +33,8 @@ class Anagrams
 
 end
 
-# ana = Anagrams.new
-# ana.check_length('mace', ['came', 'dog', 'Emma', 'base', 'best', 'pot',\
-#    'sloppy', 'camel', 'remain', 'scare'])
+ana = Anagrams.new
+ana.split('mace')
+ana.check_length('mace', ['came', 'dog', 'Emma', 'base', 'best', 'pot', 'acme',\
+   'sloppy', 'camel', 'remain', 'scare'])
+ana.sort_letters('mace')

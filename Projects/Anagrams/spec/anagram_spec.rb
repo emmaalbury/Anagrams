cb62e16d9017d9231ed48832db975332478a3fb4
Emma Albury <emma_albury@hotmail.com>
2018-12-13 14:51:25 +0000

    sort letters test

--- Projects/Anagrams/spec/anagram_spec.rb
+++ Projects/Anagrams/spec/anagram_spec.rb
@@ -4,22 +4,23 @@ describe Anagrams do
 
   let(:anagram) { described_class.new }
 
-  context 'splits' do
-    describe "#split" do
-      it "splits word string" do
-        expect(anagram.split('dog')).to eq(['d', 'o', 'g'])
-      end
+  describe "#split" do
+    it "splits word string" do
+      expect(anagram.split('dog')).to eq([['d', 'o', 'g']])
     end
-
-    # describe '#split_list' do
-    #   it "splits list"
-    # end
   end
 
   describe "#check_length" do
     it "matches length of words" do
-      expect(anagram.check_length('mace', ['came', 'dog', 'Emma', 'base', 'best', 'pot',\
-         'sloppy', 'camel', 'remain', 'scare'])).to eq ['came', 'Emma', 'base', 'best']
+      expect(anagram.check_length('mace', ['came', 'dog', 'Emma', 'base', 'best', 'pot', 'acme',\
+         'sloppy', 'camel', 'remain', 'scare'])).to eq ['came', 'Emma', 'base', 'best', 'acme']
+    end
+  end
+
+  describe '#sort_letters' do
+    it "sorts letters in words alphabetically" do
+      anagram.instance_variable_set(:@length_list, ['came', 'Emma', 'base', 'best', 'acme'])
+      expect(anagram.sort_letters('mace')).to eq ['acem', 'aEmm', 'abes','best','acem']
     end
   end
 

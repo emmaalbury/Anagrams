ff4647e3143d11d804d9b04dd9325d6477bc5119
Emma Albury <emma_albury@hotmail.com>
2018-12-31 19:13:28 +0000

    Functioning anagram method

--- Projects/Anagrams/lib/anagram.rb
+++ Projects/Anagrams/lib/anagram.rb
@@ -2,14 +2,16 @@ class Anagrams
 
   def initialize
     @length_list = []
-    @split_word = []
+    # @split_word = []
+    @sorted_word = ''
     @sorted_list = {}
+    @anagram_list = []
   end
 
-  def split(word)
-    @split_word << word.split('')
-    @split_word
-  end
+  # def split(word)
+  #   @split_word << word.downcase.split('')
+  #   @split_word
+  # end
 
   def check_length(word, list)
     list.each do |w|
@@ -20,18 +22,32 @@ class Anagrams
     @length_list
   end
 
-  def sort_letters(word)
-    sorted_word = word.chars.sort(&:casecmp).join
+  def sort_word(word)
+    @sorted_word = word.chars.sort.join
+  end
+
+  def sort_letters
     @length_list.each do |w|
-      @sorted_list[w] = w.chars.sort(&:casecmp).join
+      @sorted_list[w] = w.downcase.chars.sort.join
     end
-    p @sorted_list
+    @sorted_list
+  end
+
+  def anagrams
+    @sorted_list.each do |key, value|
+      if @sorted_word == value
+        @anagram_list << key
+      end
+    end
+    puts @anagram_list
   end
 
 end
 
 ana = Anagrams.new
-ana.split('mace')
+# ana.split('mace')
 ana.check_length('mace', ['came', 'dog', 'Emma', 'base', 'best', 'pot', 'acme',\
-   'sloppy', 'camel', 'remain', 'scare'])
-ana.sort_letters('mace')
+   'sloppy', 'camel', 'remain', 'scare', 'Meca'])
+ana.sort_word('mace')
+ana.sort_letters
+ana.anagrams
